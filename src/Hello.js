e7243300e03e422c1c4b03c2bf8470a6b0d4e486
l7g <laurent.gagne@icloud.com>
2022-08-25 12:43:11 +0200

    esercizio completo

--- src/Hello.js
+++ src/Hello.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import { Message } from './message';
+import React from "react";
+import { Message } from "./message";
 
-export class Hello extends React.Component{
-    render() {
-        return (
-            <h1>Hello, World!
-            < Message/>
-            </h1>
-        )
-    }
+export class Hello extends React.Component {
+  render() {
+    return (
+      <h1>
+        Hello, World!
+        <Message />
+      </h1>
+    );
+  }
 }

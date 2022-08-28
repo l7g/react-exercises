1b9d08dc17e244fa8d8e8897661127e2931ea34e
l7g <laurent.gagne@icloud.com>
2022-08-28 12:05:59 +0200

    esercizio completato

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import { Hello } from "./Hello";
 import React from "react";
-import { Message } from "./message";
-import { Welcome } from "./Welcome";
-import { Counter } from "./Counter";
-import { ClickTracker } from "./ClickTracker";
+import { InteractiveWelcome } from "./InteractiveWelcome";
+//import { Message } from "./message";
+//import { Welcome } from "./Welcome";
+//import { Counter } from "./Counter";
+//import { ClickTracker } from "./ClickTracker";
 
 export class App extends React.Component{
     render() {
         return (
             <div>
                 <Hello/>
-                <Message/>
+                <InteractiveWelcome/>
+                {/* <Message/>
                 <Counter/>
                 <ClickTracker/>
-                <Welcome name={<strong>Laurent</strong>} age='20'/>
+                <Welcome name={<strong>Laurent</strong>} age='20'/> */}
             </div>
         );
     }
---
+++ src/InteractiveWelcome.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Welcome } from "./Welcome";
+
+export class InteractiveWelcome extends React.Component {
+state = {
+    username: '',
+}
+
+handleInputChange = (event) => {
+    const value = event.target.value;
+
+    this.setState({
+        username: value
+    })
+}
+
+    render() {
+        return (
+            <div>
+                <Welcome name={this.state.username}/>
+                <input name="username" type="text" value={this.state.username} onChange={this.handleInputChange}/>
+            </div>
+        )
+    }
+}

ef5e95fcd6bdff5f41ac67eeaa756a0997c3f0a9
l7g <laurent.gagne@icloud.com>
2022-09-19 08:57:45 +0200

    esercizio completato

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { Container } from "./Container";
 import { LanguageContext } from "./LanguageContext";
 import { Route, Routes } from "react-router-dom";
 import {ClickCounter} from './ClickCounter'
+import {GithubUser} from './GithubUser'
+import { useEffect } from "react";
 
 //const Numbers = [6, 90, 48, 63, 12]
 
@@ -11,6 +13,7 @@ export function App({ initialLanguage = "en" }) {
   const [language, setLanguage] = useState(initialLanguage);
   const [showCounter, setShowCounter] = useState(false);
 
+  const [actualUser, setActualUser] = useState('l7g');
 
   function handleToggle() {
     setShowCounter((s) => !s);
@@ -25,6 +28,7 @@ export function App({ initialLanguage = "en" }) {
       <Routes>
         <Route path="/" element={<Welcome name='Laurent'/>}/>
         <Route path="counter" element={<ClickCounter />}/>
+        <Route path={`users/:${actualUser}`} element={<GithubUser username={actualUser}/>}/>
       </Routes>
       </Container>
     </div>
